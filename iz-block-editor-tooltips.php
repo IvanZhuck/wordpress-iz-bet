9e2f4a82694ec3a4d16b99f6404ddea17897caab
Ivan Zhuck <i.a.zhuck@yandex.ru>
2023-08-08 10:51:50 +1000

    Added frontend styles, set up phpcs, added tooltip text trimming, added filters for CSS styles

--- iz-block-editor-tooltips.php
+++ iz-block-editor-tooltips.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Plugin Name: IZ Block Editor Tooltips
  * Description: An easy way to implement tooltip element in the WordPress Block Editor
@@ -16,15 +14,30 @@ declare(strict_types=1);
  * Domain Path: /languages
  */
 
-define('IZ_BET_PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
-define('IZ_BET_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
+declare(strict_types=1);
 
-add_action('admin_enqueue_scripts', function() {
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+if (!defined('IZ_BET_PLUGIN_DIR_URL')) {
+    define('IZ_BET_PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
+}
+
+if (!defined('IZ_BET_PLUGIN_DIR_PATH')) {
+    define('IZ_BET_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
+}
+
+/**
+ * Enqueues admin assets
+ */
+function iz_enqueue_admin_assets(): void
+{
     $editorScriptUrl = IZ_BET_PLUGIN_DIR_URL . 'assets/build/js/editor.js';
     $editorScriptAssetPath = IZ_BET_PLUGIN_DIR_PATH . 'assets/build/js/editor.asset.json';
 
-    $editorStyleUrl = IZ_BET_PLUGIN_DIR_URL . 'assets/build/styles/style.css';
-    $editorStylePath = IZ_BET_PLUGIN_DIR_PATH . 'assets/build/styles/style.css';
+    $editorStyleUrl = IZ_BET_PLUGIN_DIR_URL . 'assets/build/styles/editor.css';
+    $editorStylePath = IZ_BET_PLUGIN_DIR_PATH . 'assets/build/styles/editor.css';
 
     $editorScriptMeta = json_decode(file_get_contents($editorScriptAssetPath));
 
@@ -36,6 +49,36 @@ add_action('admin_enqueue_scripts', function() {
     );
 
     wp_enqueue_style('iz-bet-editor', $editorStyleUrl, [], filemtime($editorStylePath));
-});
+}
 
+/**
+ * Enqueues common (frontend and backend) assets
+ */
+function iz_enqueue_common_assets(): void
+{
+    $styleUrl = IZ_BET_PLUGIN_DIR_URL . 'assets/build/styles/styles.css';
+    $stylePath = IZ_BET_PLUGIN_DIR_PATH . 'assets/build/styles/styles.css';
 
+    wp_enqueue_style('iz-bet-styles', $styleUrl, [], filemtime($stylePath));
+}
+
+/**
+ * Prints CSS variables to page header
+ */
+function iz_render_head_styles(): void
+{
+    echo '<style>
+            .iz-tooltip{
+                --text-color: ' . apply_filters('iz_bet_style_text_color', '#247DE0') . ';
+                --border-color: ' . apply_filters('iz_bet_style_border_color', '#247DE0') . ';
+                --tooltip-text-color: ' . apply_filters('iz_bet_style_tooltip_text_color', '#fff') . ';
+                --tooltip-background-color: ' . apply_filters('iz_bet_style_tooltip_background_color:', 'rgba(0, 010, 030, .85)') . ';
+            }
+        </style>';
+}
+
+add_action('admin_enqueue_scripts', 'iz_enqueue_admin_assets');
+add_action('admin_enqueue_scripts', 'iz_enqueue_common_assets');
+add_action('wp_enqueue_scripts', 'iz_enqueue_common_assets');
+add_action('wp_head', 'iz_render_head_styles');
+add_action('admin_head', 'iz_render_head_styles');
