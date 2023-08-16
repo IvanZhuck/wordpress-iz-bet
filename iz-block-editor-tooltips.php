99ef7c993d29811a720a9d565bdac032659c7ba6
Ivan Zhuck <i.a.zhuck@yandex.ru>
2023-08-16 19:03:58 +1000

    Updated versions

--- iz-block-editor-tooltips.php
+++ iz-block-editor-tooltips.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: IZ Block Editor Tooltips
  * Description: An easy way to implement tooltip element in the WordPress Block Editor
- * Version: 2.0.1
+ * Version: 3.0.0
  * Requires at least: 5.9
  * Requires PHP: 7.4
  * Author: Ivan Zhuck
